0fcd0307b448f6d62c27acccff5eda7ee4bdd760
Jan <mail@janchristlieb.de>
2018-03-05 18:31:24 +0100

    init project

---
+++ app/Model/Expense.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+class Expense
+{
+    public function __construct()
+    {
+        echo 'This is the expense constructor';
+    }
+}
---
+++ web/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Model;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$expense = new Model\Expense();
