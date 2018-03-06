dc9e4fae207f2d916071596d97401c3cedba44e4
Jan Christlieb <mail@janchristlieb.com>
2018-03-06 08:32:47 +0000

    defined routes

--- web/index.php
+++ web/index.php
@@ -10,5 +10,17 @@ require_once dirname(__DIR__) . '/app/bootstrap.php';
 
 Router::init();
 
+// route to dashboard 
+Router::add('/', [\App\Controller\IndexController::class, 'index']);
 
+// route to expense
+Router::add('/expense', [\App\Controller\ExpenseController::class, 'index']);
+
+// 404page
+Router::add404(function () {
+    echo 'No way, this is not a valid path (yet)';
+});
+
+Router::run();
+exit;
 
