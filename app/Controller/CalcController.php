d3c99dcf13c885b17301d2ac00a72e7b771d07e8
Jan <mail@janchristlieb.de>
2018-03-07 14:28:03 +0100

    added calcController

--- app/Controller/CalcController.php
+++ app/Controller/CalcController.php
@@ -2,10 +2,27 @@
 
 namespace App\Controller;
 
+use App\Model\User;
+
 class CalcController extends Controller
 {
-    public function index()
+    public function selectUser()
     {
-        echo $this->twig->render('/pages/calc-1.twig', ['name' => 'World']);
+        echo $this->twig->render('/pages/calc-1.twig', ['users' => User::all()]);
+    }
+
+    public function selectTimeFrame()
+    {
+        echo $this->twig->render('/pages/calc-2.twig');
+    }
+
+    public function selectDaysOff()
+    {
+        echo $this->twig->render('/pages/calc-3.twig');
+    }
+
+    public function calcFairShare()
+    {
+        echo $this->twig->render('/pages/calc-4.twig');
     }
 }
