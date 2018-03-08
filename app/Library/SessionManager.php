2c91c00546f102152126608d8c68e4784b6059e7
Jan <mail@janchristlieb.de>
2018-03-08 16:19:55 +0100

    added a class to deal with sessions

--- app/Library/SessionManager.php
+++ app/Library/SessionManager.php
@@ -4,13 +4,20 @@ namespace App\Library;
 
 class SessionManager
 {
+    public function __construct()
+    {
+        session_start();
+    }
+
     public static function set($key, $value)
     {
         //set something in session
+        $_SESSION[$key] = $value;
     }
 
     public static function get($key)
     {
         //return the value from the session
+        return $_SESSION[$key];
     }
 }
